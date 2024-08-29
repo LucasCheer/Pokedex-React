0e660acc16401650536ce848cdd88a93ba490f6c
Lucas <cheroyonatan2@gmail.com>
2024-08-28 23:22:49 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Soy un proyecto en React</h1>
+    <p>Lorem ipsum dolor sit amet consectetur.</p>
+    </>
+  )
+}
+
+export default App
