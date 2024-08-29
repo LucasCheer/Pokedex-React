b0fc6330b68f406e3c23e032429b810f44119e51
Lucas <cheroyonatan2@gmail.com>
2024-08-29 20:46:37 -0300

    armo el Footer

---
+++ src/assets/componets/Footer.jsx
@@ -0,0 +1,13 @@
+// Componente Footer
+
+import '../styles/Footer.css'
+
+export default function Footer(){
+    return (
+        <>
+        <footer>
+            <p>Desarrollado por Lucas Cheer ©</p>
+        </footer>
+        </>
+    )
+}
