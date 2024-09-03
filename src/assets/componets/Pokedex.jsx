6a48ca46f87f777c371ef24648af5bf640a88770
Lucas <cheroyonatan2@gmail.com>
2024-09-02 23:26:21 -0300

    agrego funcion para obtener datos de API

--- src/assets/componets/Pokedex.jsx
+++ src/assets/componets/Pokedex.jsx
@@ -1,6 +1,6 @@
 import '../styles/Pokedex.css'
 
-export default function Pokedex(){
+export default function Pokedex({namePokemon, numPokemon}){
     return(
         <>
         <main className='pokedex'>
@@ -8,8 +8,8 @@ export default function Pokedex(){
                 <img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/025.png" alt="Pokemon" />
             </figure>
             <div className='main-pokemon'>
-                <h2 className='name-pokemon'>Pikachu</h2>
-                <span className='num-pokemon'>#39</span>
+                <h2 className='name-pokemon'>{namePokemon}</h2>
+                <span className='num-pokemon'>#{numPokemon}</span>
                 <p className='info-pokemon'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique tenetur quam facere saepe! Debitis sequi aliquid, odit cumque recusandae dolorum?</p>
             </div>
         </main>
